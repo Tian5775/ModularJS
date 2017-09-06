39500538e514eb170e94527317a69ad938abe19d
pointAt-Tian <m18825070504@163.com>
2017-09-06 18:34:30 +0800

    20170906 pointAt-Tian 修改TestModularJs项目

--- TestModularJs/plugins/c.js
+++ TestModularJs/plugins/c.js
@@ -1,23 +1,11 @@
 // JavaScript Document
-require.config({
-	paths: {
-		"c":"../plugins/c"
-	}
-});
-
-
 define(
-	[
-		"jquery",
-		"c"
-	],
 
-	function($,c){
-		var num = 11;
+	function($){
+		var num = 22;
 		var ale = function(){
 			console.log("b");	
 			console.log($("p").text());
-			console.log(c.num);
 		}
 	
 		return{
